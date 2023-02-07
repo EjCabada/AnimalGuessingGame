9e8b9573583e727a965ce40a581e89f2d4f8d71a
EJ Cabada <cabada.eric7@gmail.com>
2023-02-07 13:27:07 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const reset = document.querySelector('#reset');
 const submit = document.querySelector('#submit');
 const allHints = document.querySelectorAll('.hintInText');
 const hintsList = document.querySelector('#hintsList');
+const scoreCounter = document.querySelector('#counter');
+const feedbackText = document.querySelector('#feedbackArea')
 const animals = [
     "Lion",
     "Tiger",
@@ -40,8 +42,10 @@ let theAnimal = animals[Math.floor(Math.random() * 30)]
 
 
 
-//Start of the Game or Reload
+//Start of the Game Parameters
 alert(`this is the animal: ${theAnimal}`)
+let score = 000
+scoreCounter.innerHTML = `Your Score is: 000`
 
 
 
@@ -54,6 +58,9 @@ submit.addEventListener('click', function () {
         input.value = '';
         allHints.forEach(hint => hint.remove());
         hintsList.innerHTML = '';
+        score = score + 100
+        scoreCounter.innerHTML = `Your Score is: ${score}`
+        feedbackText.innerText = ''
         theAnimal = animals[Math.floor(Math.random() * 25)]
         alert(`this is the animal: ${theAnimal}`)
 
@@ -66,35 +73,12 @@ submit.addEventListener('click', function () {
             hint.style.margin = '.4em';
             hint.setAttribute('class', 'hintInText');
             input.value = '';
-
-            let hint1 = document.querySelectorAll('.hintInText')[0];
-            hint1.innerHTML = `The first letter of the animal's name ${theAnimal.charAt(0)}`;
-            let hint2 = document.querySelectorAll('.hintInText')[1];
-            if (["Whale", "Dolphin", "Shark", "Otter", "Seal"].includes(theAnimal)) {
-                hint2.innerText = 'This animal lives in the ocean';
-            } else {
-                hint2.innerText = 'This animal doesn\'t live in the ocean';
-            }
-            let hint3 = document.querySelectorAll('.hintInText')[2];
-            if (theAnimal == "Pigeon" || theAnimal == "Parrot" || theAnimal == "Eagle" || theAnimal == "Woodpecker" || theAnimal == "Crow") {
-                hint3.innerText = 'This animal is a bird';
-            } else {
-                hint3.innerText = 'This animal is not a bird';
-            }
-            let hint4 = document.querySelectorAll('.hintInText')[3];
-            hint4.innerHTML = `The second letter of the animal's name ${theAnimal.charAt(1)}`;
-            let hint5 = document.querySelectorAll('.hintInText')[4];
-            hint5.innerHTML = `The name of the animal is ${theAnimal.length} leters long`;
-            let hint6 = document.querySelectorAll('.hintInText')[5];
-            hint6.innerHTML = `The third letter of the animal's name ${theAnimal.charAt(2)}`;
-            let hint7 = document.querySelectorAll('.hintInText')[6];
-            hint7.innerHTML = `Sorry no more hints ¯\\_(ツ)_/¯`;
+            feedbackText.innerText = 'Wrong guess, please try again'
+            hintsFunction()
 
         }
     }
 });
-
-
 
 
 //Reset Button Events
@@ -102,6 +86,59 @@ reset.addEventListener('click', function () {
     input.value = '';
     allHints.forEach(hint => hint.remove());
     hintsList.innerHTML = '';
+    randAnimalReset()
+});
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+//Reset counter and give new animal
+const randAnimalReset = () => {
+    score = 000
+    scoreCounter.innerHTML = `Your Score is: 000`
     theAnimal = animals[Math.floor(Math.random() * 25)]
     alert(`this is the animal: ${theAnimal}`)
-});
+    feedbackText.innerText = ''
+}
+
+const hintsFunction = () => {
+    let hint1 = document.querySelectorAll('.hintInText')[0];
+    hint1.innerHTML = `The first letter of the animal's name ${theAnimal.charAt(0)}`;
+    let hint2 = document.querySelectorAll('.hintInText')[1];
+    if (["Whale", "Dolphin", "Shark", "Otter", "Seal"].includes(theAnimal)) {
+        hint2.innerText = 'This animal lives in the ocean';
+    } else {
+        hint2.innerText = 'This animal doesn\'t live in the ocean';
+    }
+    let hint3 = document.querySelectorAll('.hintInText')[2];
+    if (theAnimal == "Pigeon" || theAnimal == "Parrot" || theAnimal == "Eagle" || theAnimal == "Woodpecker" || theAnimal == "Crow") {
+        hint3.innerText = 'This animal is a bird';
+    } else {
+        hint3.innerText = 'This animal is not a bird';
+    }
+    let hint4 = document.querySelectorAll('.hintInText')[3];
+    hint4.innerHTML = `The second letter of the animal's name ${theAnimal.charAt(1)}`;
+    let hint5 = document.querySelectorAll('.hintInText')[4];
+    hint5.innerHTML = `The name of the animal is ${theAnimal.length} leters long`;
+    let hint6 = document.querySelectorAll('.hintInText')[5];
+    hint6.innerHTML = `The third letter of the animal's name ${theAnimal.charAt(2)}`;
+    let hint7 = document.querySelectorAll('.hintInText')[6];
+    hint7.innerHTML = `Sorry no more hints ¯\\_(ツ)_/¯`;
+}
+
+
+
+
+
+
+
+
+
+        // const giveFeedback = document.createElement('p');
+        // giveFeedback.setAttribute('class', 'feedbackText');
+        // giveFeedback.innerText = 'Wrong guess, try again!';
+        // giveFeedback.style.color = 'red';
+        // giveFeedback.style.fontSize = '.4em'
+        // guessInput.appendChild(giveFeedback);
